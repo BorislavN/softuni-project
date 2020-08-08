0de52ab2d8cdbbb05ca4544a3166cb620692605a
BorislavN <borko_nik@abv.bg>
2020-08-09 00:06:33 +0300

    Added few more test. Project line coverage now equal to 82%

--- src/test/java/bg/softuni/IntegrationTests.java
+++ src/test/java/bg/softuni/IntegrationTests.java
@@ -6,6 +6,7 @@ import bg.softuni.model.entity.Offer;
 import bg.softuni.model.entity.Photo;
 import bg.softuni.model.entity.User;
 import bg.softuni.model.enumeration.Category;
+import bg.softuni.repository.CommentRepository;
 import bg.softuni.repository.ItemRepository;
 import bg.softuni.repository.OfferRepository;
 import bg.softuni.repository.UserRepository;
@@ -15,14 +16,19 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.math.BigDecimal;
 import java.util.Set;
 
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -38,6 +44,9 @@ public class IntegrationTests {
     private UserRepository userRepository;
     @Autowired
     private ItemRepository itemRepository;
+    @Autowired
+    private CommentRepository commentRepository;
+
 
     @BeforeEach
     public void init() {
@@ -54,6 +63,7 @@ public class IntegrationTests {
 
     @AfterEach
     public void destroy() {
+        this.commentRepository.deleteAll();
         this.offerRepository.deleteAll();
         this.itemRepository.deleteAll();
     }
@@ -61,8 +71,7 @@ public class IntegrationTests {
     @Test
     @WithMockUser(username = "admin", roles = {"USER", "ADMIN"})
     public void testEditRolesPageWithAdmin() throws Exception {
-        this.mockMvc.perform(MockMvcRequestBuilders
-                .get("/users/roles/edit"))
+        this.mockMvc.perform(get("/users/roles/edit"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("edit-role"));
     }
@@ -70,15 +79,13 @@ public class IntegrationTests {
     @Test
     @WithMockUser(username = "user")
     public void testEditRolesPageWithUser() throws Exception {
-        this.mockMvc.perform(MockMvcRequestBuilders
-                .get("/users/roles/edit"))
+        this.mockMvc.perform(get("/users/roles/edit"))
                 .andExpect(status().isForbidden());
     }
 
     @Test
     public void testGetOffers() throws Exception {
-        this.mockMvc.perform(MockMvcRequestBuilders
-                .get("/offers"))
+        this.mockMvc.perform(get("/offers"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("offers"))
                 .andExpect(model().attributeExists("offers"))
@@ -90,8 +97,7 @@ public class IntegrationTests {
 
     @Test
     public void testGetOffersWithTooLargePage() throws Exception {
-        this.mockMvc.perform(MockMvcRequestBuilders
-                .get("/offers").queryParam("page", "5"))
+        this.mockMvc.perform(get("/offers").queryParam("page", "5"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("offers"))
                 .andExpect(model().attributeExists("offers"))
@@ -103,16 +109,14 @@ public class IntegrationTests {
 
     @Test
     public void testGetOffersWithNegativePage() throws Exception {
-        this.mockMvc.perform(MockMvcRequestBuilders
-                .get("/offers").queryParam("page", "-55"))
+        this.mockMvc.perform(get("/offers").queryParam("page", "-55"))
                 .andExpect(status().isFound())
                 .andExpect(view().name("redirect:/error"));
     }
 
     @Test
     public void testGetOffersWithAwardsCategory() throws Exception {
-        this.mockMvc.perform(MockMvcRequestBuilders
-                .get("/offers/categories/{cat}", "awards"))
+        this.mockMvc.perform(get("/offers/categories/{cat}", "awards"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.content.[0].itemName", is("ITEM")))
                 .andExpect(jsonPath("$.number", is(0)))
@@ -121,8 +125,7 @@ public class IntegrationTests {
 
     @Test
     public void testGetOffersWithAwardsCategoryWithLargePage() throws Exception {
-        this.mockMvc.perform(MockMvcRequestBuilders
-                .get("/offers/categories/{cat}", "awards").param("page", "50"))
+        this.mockMvc.perform(get("/offers/categories/{cat}", "awards").param("page", "50"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.content.[0].itemName", is("ITEM")))
                 .andExpect(jsonPath("$.number", is(0)))
@@ -131,8 +134,7 @@ public class IntegrationTests {
 
     @Test
     public void testGetOffersWithAwardsCategoryWithNegativePage() throws Exception {
-        this.mockMvc.perform(MockMvcRequestBuilders
-                .get("/offers/categories/{cat}", "awards").param("page", "-50"))
+        this.mockMvc.perform(get("/offers/categories/{cat}", "awards").param("page", "-50"))
                 .andExpect(status().isFound())
                 .andExpect(view().name("redirect:/error"))
                 .andExpect(flash().attribute("flag", "Page index must not be less than zero!"));
@@ -140,10 +142,72 @@ public class IntegrationTests {
 
     @Test
     public void testGetOffersWithAwardsCategoryWithWrongCat() throws Exception {
-        this.mockMvc.perform(MockMvcRequestBuilders
-                .get("/offers/categories/{cat}", "test"))
+        this.mockMvc.perform(get("/offers/categories/{cat}", "test"))
                 .andExpect(status().isFound())
                 .andExpect(view().name("redirect:/error"))
                 .andExpect(flash().attribute("flag", "test is not a valid value!"));
+    }
+
+    @Test
+    public void testGetCommentsForOffer() throws Exception {
+        String id = this.offerRepository.findAll().get(0).getId();
+        this.mockMvc.perform(get("/comments/offers/{id}", id))
+                .andExpect(status().isOk())
+                .andExpect(view().name("comments"))
+                .andExpect(model().attributeExists("offer"))
+                .andExpect(model().attributeExists("comments"));
+    }
+
+    @Test
+    public void testGetCommentsForOfferWithWrongId() throws Exception {
+        this.mockMvc.perform(get("/comments/offers/{id}", "1234"))
+                .andExpect(view().name("redirect:/error"))
+                .andExpect(status().isFound())
+                .andExpect(flash().attributeExists("flag"))
+                .andExpect(flash().attribute("flag", "Offer not found!"));
+    }
+
+    @Test
+    @WithUserDetails(value = "admin")
+    public void testPostComments() throws Exception {
+        String id = this.offerRepository.findAll().get(0).getId();
+
+        this.mockMvc.perform(post("/comments/offers/{id}/add", id)
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("content", "testtext"))
+                .andExpect(view().name("redirect:/comments/offers/" + id))
+                .andExpect(status().is3xxRedirection());
+
+        assertEquals(1, this.commentRepository.count());
+    }
+
+    @Test
+    @WithUserDetails(value = "admin")
+    public void testPostCommentsWithInvalidInput() throws Exception {
+        String id = this.offerRepository.findAll().get(0).getId();
+
+        this.mockMvc.perform(post("/comments/offers/{id}/add", id)
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("content", ""))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/comments/offers/" + id))
+                .andExpect(flash().attributeExists("formModel"))
+                .andExpect(flash().attributeExists("org.springframework.validation.BindingResult.formModel"));
+
+        assertEquals(0, this.commentRepository.count());
+    }
+
+    @Test
+    public void testPostCommentsWithNoUser() throws Exception {
+        String id = this.offerRepository.findAll().get(0).getId();
+
+        this.mockMvc.perform(post("/comments/offers/{id}/add", id)
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("content", "testtext"))
+                .andExpect(status().isFound())
+                .andExpect(redirectedUrl("http://localhost/users/login"));
     }
 }
